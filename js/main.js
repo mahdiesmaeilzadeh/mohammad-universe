1b54e486bcd24cc3b294b2da112042979c9cf6a1
mohammad mahdi esmaeilzadeh <mmez.lahijan1388@gmail.com>
2026-08-28 01:15:37 +0330

    fixed main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 console.log("MAIN JS LOADED");
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
 
     const starsContainer = document.querySelector(".stars");
 
+    console.log("Stars container:", starsContainer);
+
     if (!starsContainer) {
-        console.log("Stars container not found");
         return;
     }
 
@@ -22,17 +23,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const size = Math.random() * 3 + 1;
 
-        star.style.width = ${size}px;
-        star.style.height = ${size}px;
 
-        star.style.left = ${Math.random() * 100}%;
-        star.style.top = ${Math.random() * 100}%;
+        star.style.width = size + "px";
+        star.style.height = size + "px";
 
-        star.style.animationDelay = ${Math.random() * 5}s;
-        star.style.animationDuration = ${2 + Math.random() * 4}s;
+
+        star.style.left = (Math.random() * 100) + "%";
+        star.style.top = (Math.random() * 100) + "%";
+
+
+        star.style.animationDelay =
+            (Math.random() * 5) + "s";
+
+
+        star.style.animationDuration =
+            (2 + Math.random() * 4) + "s";
 
 
         starsContainer.appendChild(star);
+
     }
 
 });
