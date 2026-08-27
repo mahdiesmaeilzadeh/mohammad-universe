fb1b470cd58e233002c1704b9ae4b339e062ef39
mohammad mahdi esmaeilzadeh <mmez.lahijan1388@gmail.com>
2026-08-28 00:59:10 +0330

    fixed stars style

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
+const starsContainer = document.querySelector(".stars");
+
+
+const starCount = 120;
+
+
+for(let i = 0; i < starCount; i++){
+
+
+    const star = document.createElement("div");
+
+    star.className = "star";
+
+
+    const size = Math.random() * 3 + 1;
+
+
+    star.style.width = ${size}px;
+    star.style.height = ${size}px;
+
+
+    star.style.left =
+    ${Math.random()*100}%;
+
+
+    star.style.top =
+    ${Math.random()*100}%;
+
+
+    star.style.animationDelay =
+    ${Math.random()*5}s;
+
+
+    star.style.animationDuration =
+    ${2 + Math.random()*4}s;
+
+
+    starsContainer.appendChild(star);
+
+}
+
+
 console.log("Mohammad Universe loaded");
